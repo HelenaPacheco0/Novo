d1df0a316a12c0b05c6e7171151ee7dd9c09853c
MariaHelenaDakic <maria.helena.dakic@users.noreply.github.com>
2021-02-08 19:29:25 +0100

    int

---
+++ src/components/Lista.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="container is-max-desktop">
+        <div class="notification is-primary">
+
+            <section class="hero is-success">
+                <div class="hero-body">
+                 <p class="title">
+                     Lorem Ipsum
+                </p>
+                <p class="subtitle">
+                    Lorem ipsum
+                </p>
+             </div>
+            </section>
+
+             <div class="block">
+               1
+            </div>
+            <div class="block">
+                2
+            </div>
+            <div class="block">
+                3
+            </div>
+            <div class="block">
+               4
+            </div>
+            <div class="block">
+              5
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.block {
+    font-familly: Roboto, sans-serif;
+    border: 1px solid #16161d;
+    margin: 20px;
+    padding: 20px;
+}
+
+.is-success {
+    background-color: #3A8F82;
+    color: white;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    
+}
+.title{
+    font-size: 30pt;
+    margin-bottom: 5px;
+}
+
+
+</style>
